43cf439e8456a40ce224df272651ee57d99a0ead
Artur Khachatryan <20770artur@gmail.com>
2023-04-22 16:46:28 +0400

    save

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,6 +7,35 @@ const getUserByID = async (req, res) => {
     res.status(200).json(new SuccessResponseDTO(user));
 }
 
+const getUsersStatistic = async (req, res) => {
+    const page = Number(req.params.page) - 1;
+    const count = Number(req.params.count);
+
+    const usersCount = await usersService.getUsersCount();
+    const pagesCount = Math.ceil(usersCount / page);
+    const users = await usersService.getUsers({
+        filter: {},
+        select: { firstName: 1, lastName: 1, rating: 1 },
+        skip: count * page,
+        limit: count,
+        sort: { rating: -1 },
+    });
+
+    res.status(200).json(new SuccessResponseDTO({ users, usersCount, pagesCount }));
+}
+
+// boandakutyun
+
+// neracutyun
+// node as backend, express, validation, jwt auth, mvc, mongodb
+// 
+
+const me = async (req, res) => {
+    return res.json(new SuccessResponseDTO(req.user));
+}
+
 export default {
     getUserByID,
+    getUsersStatistic,
+    me,
 }
