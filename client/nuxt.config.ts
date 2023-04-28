7da7a436014d4bab24a2743132c5a018a60a4296
YuraTadevosyan <yuratadevosyan01@gmail.com>
2023-04-29 01:09:52 +0400

    feat:[Title] => changed the title

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -7,6 +7,7 @@ import {resolve} from 'path';
 export default defineNuxtConfig({
   app: {
     head: {
+      title: 'A Quiz',
       link: [
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
