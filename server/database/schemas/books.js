4c7a21acef617c712b008d0e4d4e1236b569849a
Artur Khachatryan <20770artur@gmail.com>
2023-05-15 20:55:17 +0400

    change model of books

--- server/database/schemas/books.js
+++ server/database/schemas/books.js
@@ -1,6 +1,7 @@
 import { Schema } from "mongoose";
 export default new Schema(
   {
+    name: Schema.Types.String,
     imageURL: Schema.Types.String,
     bookURL: Schema.Types.String,
     category: Schema.Types.String,
