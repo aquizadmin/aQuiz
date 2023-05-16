836fa08675765175798066598640cf50ac5a35bf
Armida <armidarehanyan@mail.ru>
2023-05-16 14:08:01 +0400

    Create Books and Videos page

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import {Router} from "express";
 
 import authentication from "../middlewares/authentication.js";
 import videosController from "../controllers/videos.js";
@@ -7,6 +7,6 @@ const videosRouter = Router();
 
 videosRouter.use(authentication.authenticate);
 videosRouter.get("/:id", videosController.getVideo);
-videosRouter.get("/:page/:count/:category", videosController.getVideos);
+videosRouter.get("/:page/:count/:category?", videosController.getVideos);
 
 export default videosRouter;
